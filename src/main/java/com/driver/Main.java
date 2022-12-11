6f3e976d60089c839d7456acfd0766e39e299d25
malishrirama17@gmail.com <you@example.com>
2022-12-11 23:25:00 +0530

    message

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,19 @@
 package com.driver;
-
+class A{
+    public String meth(){
+      return"Invoking method from class A";
+    }
+}
+class B extends A{
+    @Override
+   public String meth(){
+        return"Method is overridden in Extendend class B";
+    }
+}
 public class Main {
-  
+public static void main(String[] args){
+    A obj1 = new B();
+    obj1.meth();
+}
+
 }
